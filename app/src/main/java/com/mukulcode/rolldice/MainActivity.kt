21831e6f69f0ef1a84456f125675bc8a1a88c6d0
Mukul Kumar <mukul.2121@gmail.com>
2020-10-27 21:50:11 +0530

    Added Images instead of Numbers

--- app/src/main/java/com/mukulcode/rolldice/MainActivity.kt
+++ app/src/main/java/com/mukulcode/rolldice/MainActivity.kt
@@ -3,9 +3,8 @@ package com.mukulcode.rolldice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +20,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
         //Picking a Random Value
         val randomValue = java.util.Random().nextInt(6)+1
-        resultText.text = randomValue.toString()
-
+        val drawableResource = when(randomValue){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
